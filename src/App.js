a1e2aaefec4fae199c36e0ebd611ec5033d22aa6
Suraj Gupta <8700suraj@gmail.com>
2024-12-22 18:58:18 +0530

    Scap1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1> E-Dashboard </h1>
+       <BrowserRouter> 
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={ <h1> E-Dashboard </h1>}/> 
+        <Route path='/add' element={ <h1> Add Product Components </h1>}/> 
+        <Route path='/update' element={ <h1> Update Product Components </h1>}/> 
+        <Route path='/logout' element={ <h1> Logout Components </h1>}/> 
+        <Route path='/profile' element={ <h1> Profile Components </h1>}/> 
+      </Routes>
+      
+       </BrowserRouter>  
     </div>
   );
 }
-
 export default App;
+
+ 
