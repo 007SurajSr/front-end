ee31fc7a060f10a998ae6b607f20c1eee177aa98
Suraj Gupta <8700suraj@gmail.com>
2024-12-29 17:02:13 +0530

    concurrently

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         <Route path='/add' element={ <h1> Add Product Components </h1>}/> 
         <Route path='/update' element={ <h1> Update Product Components </h1>}/> 
         <Route path='/logout' element={ <h1> Logout Components </h1>}/> 
+        <Route path='/delete' element={ <h1> Delete Components </h1>}/> 
         <Route path='/profile' element={ <h1> Profile Components </h1>}/> 
       </Routes>
       
