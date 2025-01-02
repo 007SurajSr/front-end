1405ae68b805ae39e63f0cff246580be2f1cb202
Suraj Gupta <8700suraj@gmail.com>
2025-01-02 15:11:55 +0530

    back1

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignUp = () =>{
+    const[name, setName] = useState("");
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+  
+   const collectData=()=>{
+    console.warn(name, email, password);
+   }
     return (
         <div className="register">
             <h1  >Register</h1>
-            <input className="inputBox" type="text" placeholder="Enter Name"/>
-            <input className="inputBox" type="text" placeholder="Enter Email"/>
-            <input className="inputBox" type="password" placeholder="Enter Password"/>
+            <input className="inputBox" type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter Name"/>
+            <input className="inputBox" type="text" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter Email"/>
+            <input className="inputBox" type="password"  value={password} onChange={(e)=>setPassword(e.target.value)}  placeholder="Enter Password"/>
+            <button className="appButton" onClick={collectData} type="button">Sign Up</button>
         </div>
     )
 }
