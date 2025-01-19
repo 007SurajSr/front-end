b6e5f1eacb9a81609e7397307d5d69987147de42
Suraj Gupta <8700suraj@gmail.com>
2025-01-19 14:00:18 +0530

    13D

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React  from 'react';
-import {Link } from 'react-router-dom';
+import {Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
    
     const auth = localStorage.getItem('user');
+    const navigate = useNavigate();
+   
+    const logout =()=>{
+       localStorage.clear();
+       navigate('/signup');
+    }
 
     return(
         <div>
@@ -13,7 +19,8 @@ const Navbar = () => {
                 <li><Link to="/update">Update Products</Link></li>
                 <li><Link to="/delete">Delete Products</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
-                <li>{auth ? <Link to="/logout">Logout</Link>:<Link to="/signup">Sign up Page</Link>} </li>
+                <li>{auth ? <Link onClick={logout} to="/signup">Logout</Link>:
+                <Link to="/signup">Sign Up</Link>} </li>
             </ul>
         </div>
     );
