1fb608c4f1d9e5e3146c3ce94870300dd25133e5
Suraj Gupta <8700suraj@gmail.com>
2025-01-24 12:33:53 +0530

    17D

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,21 +13,28 @@ const Navbar = () => {
 
     return(
         <div>
-            <ul className='nav-ul'>
+            <img alt='logo' 
+            className='logo'
+            src='https://previews.123rf.com/images/dariachekman/dariachekman1910/dariachekman191000015/132817556-pink-store-icon-shop-icon-flat-design-vector-illustration.jpg'/>
+            {auth ? <ul className='nav-ul'>
                 <li><Link to="/">Products</Link></li>
                 <li><Link to="/add">Add Products</Link></li>
                 <li><Link to="/update">Update Products</Link></li>
                 <li><Link to="/delete">Delete Products</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
+                <li><Link onClick={logout} to="/signup">Logout  ({JSON.parse(auth).name}) </Link></li>
+                </ul>
                 
-                {
-                    auth ? <li><Link onClick={logout} to="/signup">Logout</Link></li>
-                    :<>
+                :
+                
+                <ul className='nav-ul nav-right'>
+                   
                     <li><Link to="/signup">Sign Up</Link></li>
                     <li><Link to="/login">Login</Link></li>
-                    </>
+                
+                </ul>
                 }
-            </ul>
+           
         </div>
     );
 }
