ce1936c0511c978f8351d3a14df70e4e5f9be737
Suraj Gupta <8700suraj@gmail.com>
2025-03-06 19:22:25 +0530

    35D

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -29,7 +29,8 @@ const SignUp = () =>{
     });
     const result = await response.json();
     console.warn(result);
-    localStorage.setItem("user",JSON.stringify(result));
+    localStorage.setItem("user",JSON.stringify(result.result));
+    localStorage.setItem("token",JSON.stringify(result.auth));
     if(response){
         navigate('/');
     }
